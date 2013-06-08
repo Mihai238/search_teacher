062b8f2f941f84aa236d644a723957d86ae1cc6d
Mihai <mihailepadat@gmail.com>
2013-06-08 19:34:34 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchTeacher::Application.config.secret_token = 'b43d4d1e0f175b71ab54301c4807a28680da80315b46f651dccf42a88ea02b1b977682448cd724e6f055412e309249c7a26f0416cfec933dcb2fec0f35c3e0e0'
