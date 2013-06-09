fe7365b6bafca0b7a59989ccfb67515d53052305
Mihai <mihailepadat@gmail.com>
2013-06-09 22:55:14 +0200

    bug at routes.rb done.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,8 @@ SearchTeacher::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'index.html'
+  root :to => 'welcome#index'
+  #root :to => 'index.html'
 
   # See how all your routes lay out with "rake routes"
 
